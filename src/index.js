ccf0c188928051655adcb0473ff574b98c749283
Anuj Kumar <adakdhuriya3@gmail.com>
2024-10-18 15:20:38 +0530

    setup utils and Api Errors and some basics also

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import {app} from './app.js'
 
 dotenv.config({
     path: './env'
@@ -7,6 +8,18 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.on("Error", (error)=>{
+    console.log("Error found")
+             })
+
+   app.listen(process.env.PORT || 800, ()=>{
+    console.log(`Server is running at PORT: ${process.env.PORT}`)
+   })
+})
+.catch( (error)=>{
+console.log(`MONGODB Connection Failed`, error)
+})
 
 
 
